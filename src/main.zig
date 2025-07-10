518767aa67fd117c728a855453539591b12a8fb7
Pavel Shchichko <paul.schko@gmail.com>
2025-07-10 22:58:09 +0800

    Working lvl reader

--- src/main.zig
+++ src/main.zig
@@ -130,6 +130,14 @@ pub fn main() !void {
     // try doUnits(allocator, file, index, palette);
 
     const ops = [_]FrameBlockOp{.{ .copy = .{ .items = 4 } }};
+    const copy56 = [_]FrameBlockOp{.{ .copy = .{ .items = 56 } }};
+    const replicate56 = [_]FrameBlockOp{.{ .replicate = .{ .items = 1, .times = 56 } }};
+    const ops3 = &[_]FrameBlockOp{
+        .{ .copy = .{ .items = 34 } },
+        .{ .skip = .{ .items = 4 } },
+        .{ .copy = .{ .items = 18 } },
+    };
+    _ = ops3;
     // allocate pixels, how to get size???
     // sprite_sheet(name, w, h).from[frames(at_index)(+w,+h).chunks[], frames[at_index](+w, +h).chunks[]]
     //(read frames(+fsize), apply ops(+image_len) => chunk), write_chunk at offset(+image_size), write_pixels, write image
@@ -138,53 +146,216 @@ pub fn main() !void {
     defer frames_arena_allocator.deinit();
     const fa = frames_arena_allocator.allocator();
 
+    var tilesIndex: TilesIndex = std.AutoHashMap(u8, usize).init(allocator);
+    defer tilesIndex.deinit();
+
+    try tilesIndex.put(0xF0, 0x30); //coast
+    try tilesIndex.put(0xF3, 0x40); //light snow
+    try tilesIndex.put(0xF6, 0x60); //dark marsh
+    try tilesIndex.put(0xF7, 0x50); //light marsh
+    try tilesIndex.put(0xF8, 0x20); //dark water
+    try tilesIndex.put(0xF9, 0x10); //light water
+    try tilesIndex.put(0x09, 0x200); //coast - light water
+    try tilesIndex.put(0x20, 0x500); //light marsh - coast
+    try tilesIndex.put(0x26, 0x600); //light marsh - dark marsh
+    try tilesIndex.put(0x37, 0x300); //light snow - light marsh
+    try tilesIndex.put(0x49, 0x100); //light water - dark water
+    // 0 - coast
+    // 1 - ice
+    // 2 - dark snow
+    // 3 - light snow
+    // 4 - dark steppe
+    // 5 - dark steppe
+    // 6 - dark marsh
+    // 7 - light marsh
+    // 8 - dark water
+    // 9 - light water
+
+    // 0 - coast [with light water]
+    // 1 - light steppe
+    // 2 - light marsh
+    // 3 - light snow
+    // 4 - light water [with dark water]
+    // 5 - ice [with light water]
+    //15 - no corner needed, just base tile
+
     //indices point to images packed in a .RES file
     const sources = [_]Output{
-        .{ .transform = .{ .frames = try from(fa, file, index[3]), .ops = &ops } },                                             .{ .transform = .{ .frames = try from(fa, file, index[7]), .ops = &ops } }, .{ .transform = .{ .frames = try from(fa, file, index[8]), .ops = &ops } }, .{ .transform = .{ .frames = try from(fa, file, index[9]), .ops = &ops } }, .{ .transform = .{ .frames = try from(fa, file, index[10]), .ops = &ops } }, .{ .transform = .{ .frames = try from(fa, file, index[11]), .ops = &ops } }, .{ .transform = .{ .frames = try from(fa, file, index[12]), .ops = &ops } },
-        .{ .transform = .{ .frames = try from(fa, file, index[13]), .ops = &[_]FrameBlockOp{
-            .{ .copy = .{ .items = 34 } },
-            .{ .skip = .{ .items = 4 } },
-            .{ .copy = .{ .items = 18 } },
-        } } },
-        .{ .transform = .{ .frames = try from(fa, file, index[3]), .ops = &[_]FrameBlockOp{
-            .{ .replicate = .{ .items = 1, .times = 52 } },
-        } } },
-        .{ .offset = .{ .frames = -52 } },
-        .{ .transform = .{ .frames = try from(fa, file, index[14]), .ops = &[_]FrameBlockOp{
-            .{ .copy = .{ .items = 34 } },
-            .{ .skip = .{ .items = 4 } },
-            .{ .copy = .{ .items = 18 } },
-        } } },
-        .{ .transform = .{ .frames = try from(fa, file, index[7]), .ops = &[_]FrameBlockOp{
-            .{ .replicate = .{ .items = 1, .times = 52 } },
-        } } },
-        .{ .offset = .{ .frames = -52 } },
-        .{ .transform = .{ .frames = try from(fa, file, index[14]), .ops = &[_]FrameBlockOp{
-            .{ .copy = .{ .items = 34 } },
-            .{ .skip = .{ .items = 4 } },
-            .{ .copy = .{ .items = 18 } },
-        } } },
-        .{ .transform = .{ .frames = try from(fa, file, index[8]), .ops = &[_]FrameBlockOp{
-            .{ .replicate = .{ .items = 1, .times = 52 } },
-        } } },
-        .{ .offset = .{ .frames = -52 } },
-        .{ .transform = .{ .frames = try from(fa, file, index[15]), .ops = &[_]FrameBlockOp{
-            .{ .copy = .{ .items = 34 } },
-            .{ .skip = .{ .items = 4 } },
-            .{ .copy = .{ .items = 18 } },
-        } } },
-        .{ .transform = .{ .frames = try from(fa, file, index[17]), .ops = &[_]FrameBlockOp{.{ .copy = .{ .items = 56 } }} } },
-        .{ .transform = .{ .frames = try from(fa, file, index[23]), .ops = &[_]FrameBlockOp{.{ .copy = .{ .items = 36 } }} } },
+        .{ .transform = .{ .frames = try from(fa, file, index[3]), .ops = &ops } },
+        .{ .transform = .{ .frames = try from(fa, file, index[6]), .ops = &ops } },
+        .{ .transform = .{ .frames = try from(fa, file, index[9]), .ops = &ops } },
+        .{ .transform = .{ .frames = try from(fa, file, index[10]), .ops = &ops } },
+        .{ .transform = .{ .frames = try from(fa, file, index[11]), .ops = &ops } },
+        .{ .transform = .{ .frames = try from(fa, file, index[12]), .ops = &ops } },
+        .{ .transform = .{ .frames = try from(fa, file, index[13]), .ops = &copy56 } },
+        .{ .transform = .{ .frames = try from(fa, file, index[3]), .ops = &replicate56 } },
+        .{ .offset = .{ .frames = -56 } },
+        .{ .transform = .{ .frames = try from(fa, file, index[15]), .ops = &copy56 } },
+        .{ .transform = .{ .frames = try from(fa, file, index[9]), .ops = &replicate56 } },
+        .{ .offset = .{ .frames = -56 } },
+        .{ .transform = .{ .frames = try from(fa, file, index[15]), .ops = &copy56 } },
+        .{ .transform = .{ .frames = try from(fa, file, index[10]), .ops = &replicate56 } },
+        .{ .offset = .{ .frames = -56 } },
+        .{ .transform = .{ .frames = try from(fa, file, index[16]), .ops = &copy56 } },
+        .{ .transform = .{ .frames = try from(fa, file, index[17]), .ops = &copy56 } },
     };
 
+    // _ = sources;
     const sprite_sheet = try outputFrames(allocator, &sources, .{ .from = 88, .to = 88, .n = 8 }, 16, 512);
     defer allocator.free(sprite_sheet.pixels);
 
-    try writeImage(allocator, "swamp", sprite_sheet.width, sprite_sheet.height, palette[0..256], sprite_sheet.pixels);
+    try writeImage(allocator, "tundra", sprite_sheet.width, sprite_sheet.height, palette[0..256], sprite_sheet.pixels);
 
-    // var chunk: FrameChunk = read_frames();
-    // ops.
+    // const map_dir_name = "C:/Program Files/GOG Galaxy/Games/War Wind/Data/NETWORK";
+    // const map_dir = try std.fs.openDirAbsolute(map_dir_name, .{ .iterate = true });
+    // var files = map_dir.iterate();
+    // while (try files.next()) |f| {
+    //     switch (f.kind) {
+    //         .file => {
+    //             std.debug.print("{s}", .{f.name});
+    //             const full_file_name = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ map_dir_name, f.name });
+    //             defer allocator.free(full_file_name);
+    //             var file1 = try std.fs.cwd().openFile(full_file_name, .{ .mode = .read_only });
+    //             defer file1.close();
+    //             try getTerrain(allocator, file1);
+    //         },
+    //         else => continue,
+    //     }
+    // }
+
+    const lvlfile = "C:/Program Files/GOG Galaxy/Games/War Wind/Data/NETWORK/BLANK01.LVL";
+    var file1 = try std.fs.cwd().openFile(lvlfile, .{ .mode = .read_only });
+    defer file1.close();
+
+    try getTerrain(allocator, file1, tilesIndex);
 }
+
+const TilesIndex = std.AutoHashMap(u8, usize);
+pub fn getTerrain(allocator: std.mem.Allocator, file: std.fs.File, tilesIndex: TilesIndex) !void {
+    const data = file.reader();
+
+    const header: [6]u8 = try data.readBytesNoEof(6);
+    // std.debug.print("{s}\n", .{header});
+    std.debug.assert(std.mem.eql(u8, &header, "WARMAP"));
+
+    //Should be 1
+    const levelInfo = try data.readInt(u32, .little);
+    std.debug.assert(levelInfo == 1);
+    // std.debug.print("levelInfo: {d}\n", .{levelInfo});
+
+    // base tiles as of RES.005
+    // in_lvl_file offset_res.005 name
+    // 0   1 - coast
+    // 1   2 - ice
+    // 2   3 - dark snow
+    // 3   4 - light snow
+    // 4   5 - dark steppe
+    // 5   6 - dark steppe
+    // 6   7 - dark marsh
+    // 7   8 - light marsh
+    // 8   9 - dark water
+    // 9  10 - light water
+    var base_tiles: []u8 = undefined;
+    defer allocator.free(base_tiles);
+    {
+        const record_size_bytes = try data.readInt(u32, .little);
+        const records = try data.readInt(u32, .little);
+        _ = record_size_bytes;
+        base_tiles = try allocator.alloc(u8, records);
+        // std.debug.print("{d}:{d}\n", .{ record_size_bytes, records });
+
+        std.debug.print("base tiles set: ", .{});
+        for (0..records) |i| {
+            const x: u8 = try data.readInt(u8, .little);
+            base_tiles[i] = x;
+            std.debug.print("{d}, ", .{x});
+        }
+        // std.debug.print("\n", .{});
+    }
+
+    // corner tiles as of RES.005
+    // in_lvl_file offset_res.005 name
+    // 0  19 - coast [with light water]
+    // 1  20 - light steppe
+    // 2  21 - light marsh
+    // 3  22 - light snow
+    // 4  23 - light water [with dark water]
+    // 5  24 - ice [with light water]
+    //15  15 - no corner needed, just base tile
+    var corner_tiles: []u8 = undefined;
+    defer allocator.free(corner_tiles);
+    {
+        const record_size_bytes = try data.readInt(u32, .little);
+        const records = try data.readInt(u32, .little);
+        _ = record_size_bytes;
+        corner_tiles = try allocator.alloc(u8, records);
+        // std.debug.print("{d}:{d}\n", .{ record_size_bytes, records });
+
+        std.debug.print("corner tiles set: ", .{});
+        for (0..records) |i| {
+            const x: u8 = try data.readInt(u8, .little);
+            corner_tiles[i] = x;
+            std.debug.print("{d}, ", .{x});
+        }
+        std.debug.print("\n", .{});
+    }
+
+    var map_terrain: []MapTile = undefined;
+    defer allocator.free(map_terrain);
+    {
+        const record_size_bytes = try data.readInt(u32, .little);
+        const records = try data.readInt(u32, .little);
+        map_terrain = try allocator.alloc(MapTile, records);
+        std.debug.print("Records: {d}, record size: {d}\n", .{ records, record_size_bytes });
+
+        // std.debug.print("map terrain: \n", .{});
+        for (0..(records), 0..) |i, n| {
+            const x: u16 = try data.readInt(u16, .little);
+            var tile = @as(MapTile, @bitCast(x));
+            tile.base_tile = @as(u4, @intCast(base_tiles[tile.base_tile]));
+            if (tile.corner_tile != 0xF) {
+                tile.corner_tile = @as(u4, @intCast(corner_tiles[tile.corner_tile]));
+            }
+            map_terrain[i] = tile;
+            if (n < 10) {
+                std.debug.print("code: {x}, base: {d}:{d}, corner: {d}:{d}\n", .{ x, tile.base_tile, tile.base_tile_index, tile.corner_tile, tile.corner_tile_index });
+            }
+        }
+        // std.debug.print("\n", .{});
+    }
+
+    const outfile_name = "C:/Projects/map.txt";
+    var f = try std.fs.createFileAbsolute(outfile_name, .{ .truncate = true });
+    f.close();
+    var outfile = try std.fs.openFileAbsolute("C:/Projects/map.txt", .{ .mode = .write_only });
+    defer outfile.close();
+    const writer = outfile.writer();
+    for (map_terrain, 0..) |tile, i| {
+        const key: u8 = @as(u8, @intCast(tile.corner_tile)) << 4 | @as(u8, @intCast(tile.base_tile));
+        if (tilesIndex.get(key)) |tile_group_offset| {
+            const row = (i / 96) * 2;
+            const column = (i % 96) * 2;
+            const tile_frame_offset = if (tile.corner_tile == 0xF) @as(usize, tile.base_tile_index) else @as(usize, tile.corner_tile_index);
+            const tile_number = tile_group_offset + tile_frame_offset;
+
+            const s = try std.fmt.allocPrint(allocator, "SetTile( {d}, {d}, {d}, 0, 0)\n", .{ tile_number, column, row });
+            if (i < 10) {
+                std.debug.print("SetTile( {d}, {d}, {d}, 0, 0)\n", .{ tile_number, column, row });
+            }
+            _ = try writer.write(s);
+            allocator.free(s);
+        } else {
+            std.debug.print("Can't get tile offset for key {x}\n", .{key});
+        }
+    }
+}
+
+const MapTile = packed struct {
+    base_tile_index: u2,
+    base_tile: u4,
+    corner_tile_index: u6,
+    corner_tile: u4,
+};
 
 pub fn from(a: std.mem.Allocator, file: std.fs.File, offset: u32) ![]const Frame {
     try file.seekTo(offset);
@@ -213,7 +384,7 @@ pub fn readIndex(a: std.mem.Allocator, data: std.fs.File.Reader) ![]u32 {
     for (0..num_nodes) |i| {
         const next_node_position: u32 = try data.readInt(u32, .little);
         index[i] = next_node_position;
-        std.debug.print("Node {d}: offset: {X}\n", .{ i, next_node_position });
+        // std.debug.print("Node {d}: offset: {X}\n", .{ i, next_node_position });
         // try bw.flush(); // Don't forget to flush!
         // try data.skipBytes(next_node_position - current_position, .{});
     }
@@ -578,7 +749,7 @@ fn write_chunk(chunk: *FrameChunk, storage: []u8, cols: usize, offset_frames: us
         }
 
         for (start_col..end_col) |j| {
-            std.debug.print("Put frame {d}:{d}, frame size: {d}x{d}\n", .{ i, j, chunk.frame_w, chunk.frame_h });
+            // std.debug.print("Put frame {d}:{d}, frame size: {d}x{d}\n", .{ i, j, chunk.frame_w, chunk.frame_h });
             const source_frame = frame_iterator.next().?;
             // const frame_offset_w: usize = (chunk.frame_w - source_frame.width) / 2;
             // const frame_offset_h: usize = (chunk.frame_h - source_frame.height) / 2;
